5a6929bce23c78e2f43c1ba2e46ef81c7b531967
Spencer Enriquez <44066348+spencer-enriquez@users.noreply.github.com>
2018-12-07 23:12:53 -0800

    Transfer

---
+++ Plane.java
@@ -0,0 +1,14 @@
+/**
+ * Flying airplane.
+ * @author Alvin Nguyen
+ */
+public class Plane extends Aircraft {
+	
+	public Plane(int x, int y, String imgFile) {
+		super(x, y, imgFile);
+	}
+
+	public String toString() {
+		return "Plane at:\n" + this.getHitbox().printBounds();
+	}
+}
